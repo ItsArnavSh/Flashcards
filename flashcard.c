edf56b83f1daab2aff735305837836d2abd47ab1
Arnav Sharma <its.arnav.sh@gmail.com>
2023-09-20 13:09:32 +0530

    New files

---
+++ flashcard.c
@@ -0,0 +1,43 @@
+//Creating a flashcards terminal app for a C
+#include <stdio.h>  //for printf() and scanf().
+//There will be two options, one to revise more flashcards, and another one to create more.
+//We will use the FOP approach for this and create two seperate funtions, for either case
+void create()
+{
+    //The user is prompted to either create a new deck or add cards to an existing deck
+    //If the user wants to edit an existing deck, he is sent to card append from here
+}
+void newDeck()
+{
+    //The user is prompted to enter its name
+    //The computer generates a 2 letter code for this deck.
+    //Then the user is redirected to card append
+}
+void cardappend()
+{
+    //This is used to add questions to a card
+}
+void carddelete()
+{
+    //This is used to delete a specific card from a deck
+
+}
+void deckDelete()
+{
+    //This will delete the entire deck.
+    //Not really delete, but only remove its name from the header file.
+}
+void deleter()
+{
+    //Redirected here from deck delete and 
+    //We will use a temp file, copy everything from the main file to the temp file, except for the element to be deleted
+    //After that we take the temp file and write it over the main file.
+}
+void review()
+{
+
+}
+int main(void)
+{
+
+}
