a98f9a874a4371a337762dff61f80dd0835b8eca
Arnav Sharma <its.arnav.sh@gmail.com>
2023-09-21 06:39:15 +0530

    Comments

--- flashcard.c
+++ flashcard.c
@@ -4,14 +4,21 @@
 //We will use the FOP approach for this and create two seperate funtions, for either case
 void create()
 {
-    //The user is prompted to either create a new deck or add cards to an existing deck
-    //If the user wants to edit an existing deck, he is sent to card append from here
+    //The user is prompted to either create a new deck or add cards to an existing deck or to delete an existing deck
+    //If the user wants to edit an existing deck or delete it, he is sent to display() from here that displays all the decks that are available
+    //After that, the user is sent to writer() for edit or deckDelete() in case of deletion with the code of the deck.
+    //If user wants to add a new deck, he is sent to newDeck()
 }
 void newDeck()
 {
     //The user is prompted to enter its name
     //The computer generates a 2 letter code for this deck.
     //Then the user is redirected to card append
+}
+void writer()
+{
+    //This function will generate the "flow" of control for all the data and send it
+
 }
 void cardappend()
 {
@@ -33,11 +40,17 @@ void deleter()
     //We will use a temp file, copy everything from the main file to the temp file, except for the element to be deleted
     //After that we take the temp file and write it over the main file.
 }
+//The code below this is for review purposes...literally
 void review()
 {
-
+ //The user is directed to display()
+ //The flow will return here after the display
+}
+void display()
+{
+    //This snippet is supposed to display all the files from the content file along with their codes
 }
 int main(void)
 {
-
+    
 }
